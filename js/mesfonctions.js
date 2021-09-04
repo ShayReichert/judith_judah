dbdf2ad5e8722673c4a1a31a18417ea5e48e200f
Shay Reichert <shay.reichert@outlook.fr>
2021-09-04 13:41:53 +0200

    Current Year Fix

--- js/mesfonctions.js
+++ js/mesfonctions.js
@@ -1,17 +1,18 @@
-$(function() {
-    /**
-    * Smooth scrolling to page anchor on click
-    **/
-    $("a[href*='ancre01']:not([href='ancre01'])").click(function() {
-        if (
-            location.hostname == this.hostname
-            && this.pathname.replace(/^\//,"") == location.pathname.replace(/^\//,"")
-        ) {
-            var anchor = $(this.hash);
-            anchor = anchor.length ? anchor : $("[name=" + this.hash.slice(1) +"]");
-            if ( anchor.length ) {
-                $("html, body").animate( { scrollTop: anchor.offset().top }, 1000);
-            }
-        }
-    });
+$(document).ready(function () {
+  /**
+   * Smooth scrolling to page anchor on click
+   **/
+  $("a[href*='ancre01']:not([href='ancre01'])").click(function () {
+    if (location.hostname == this.hostname && this.pathname.replace(/^\//, "") == location.pathname.replace(/^\//, "")) {
+      var anchor = $(this.hash);
+      anchor = anchor.length ? anchor : $("[name=" + this.hash.slice(1) + "]");
+      if (anchor.length) {
+        $("html, body").animate({ scrollTop: anchor.offset().top }, 1000);
+      }
+    }
+  });
+
+  // Display current year
+  const currentYearContainer = document.querySelector(".current-year");
+  currentYearContainer.appendChild(document.createTextNode(new Date().getFullYear()));
 });
